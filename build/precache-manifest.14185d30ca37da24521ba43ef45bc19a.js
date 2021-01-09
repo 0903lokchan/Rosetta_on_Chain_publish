c2f009831349030c9878e959076e2fbf956ef771
0903lokchan <0903lokchan@gmail.com>
2021-01-09 15:05:54 +1300

    initial commit

---
+++ build/precache-manifest.14185d30ca37da24521ba43ef45bc19a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77840c677ab3a8467d328d2d4358f992",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47ce1c358b26c3758a30",
+    "url": "/static/css/2.c72968ad.chunk.css"
+  },
+  {
+    "revision": "c2a3f4d569b261cf3b9e",
+    "url": "/static/css/main.206d5428.chunk.css"
+  },
+  {
+    "revision": "47ce1c358b26c3758a30",
+    "url": "/static/js/2.2bac2e4e.chunk.js"
+  },
+  {
+    "revision": "a77dfe5d27ac1d9e13bdaab8eb4b5e1d",
+    "url": "/static/js/2.2bac2e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a3f4d569b261cf3b9e",
+    "url": "/static/js/main.0eaed086.chunk.js"
+  },
+  {
+    "revision": "052eb49720fa732f8fe9",
+    "url": "/static/js/runtime-main.48ce18cb.js"
+  }
+]);
